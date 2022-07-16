7ed2f98f601c7d7166233fc4d6bdbe912a336054
Skander <skanderchebil1@gmail.com>
2022-07-16 02:15:32 +0100

    assets push, finished prototyping the header/footer sections

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <>
+      <div className='flexContainer'>
+        <div id='footerText'>
+          <h3>Terms&Privacy</h3>
+          <h3>Contact</h3>
+        </div>
+
+        <div id='contact'>
+          <h5>©2022|Light Stage|All Rights Reserved</h5>
+          <img
+            src={require("../Images/iconINSTAGRAM.png")}
+            alt='instagramIcon'
+          />
+          <img src={require("../Images/iconTWITTER.png")} alt='twitterIcon' />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <>
+      <div className='nav'>
+        <img
+          id='lightStageLogo'
+          src={require("../Images/LightStageLogo.png")}
+          alt=''
+        />
+
+        <div id='navText'>
+          <h1>EXPLORE CREATORS</h1>
+          <h2>MY PROJECTS</h2>
+        </div>
+
+        <img id='avatar' src={require("../Images/Avatar.png")} alt='' />
+      </div>
+
+      <div className='banner'>
+        <img id='Banner' src={require("../Images/Banner.jpg")} alt='' />
+        <div id='bannerText'>
+          <h1>Creative motion projects</h1>
+          <h4>Discover client work to pitch on.</h4>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
