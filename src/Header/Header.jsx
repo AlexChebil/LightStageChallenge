4ea20a65137a59c7ed182743fd6c96d2eb2e934d
Skander <skanderchebil1@gmail.com>
2022-07-17 14:10:29 +0100

    migrated some values from "%" to much stable "em"s, spa now detect the window's width and act differently depending on the device's width, slight changes to the MQs and added the official FE Challenge LightStage Banners

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import "./HeaderMediaQ.scss";
 
 function Header() {
+  const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+
+  window.addEventListener("resize", () => {
+    setCurrentWidth(window.innerWidth);
+  });
+
   return (
     <>
       <div className='nav'>
@@ -21,7 +27,23 @@ function Header() {
       </div>
 
       <div className='banner'>
-        <img id='Banner' src={require("../Images/Banner.jpg")} alt='' />
+        {
+          /* A ternary operator. It is a shorthand if/else statement. */
+          currentWidth >= 600 ? (
+            <img
+              id='BannerBig'
+              src={require("../Images/bannerBig.jpg")}
+              alt=''
+            />
+          ) : (
+            <img
+              id='BannerSmall'
+              src={require("../Images/bannerSmall.jpg")}
+              alt=''
+            />
+          )
+        }
+
         <div id='bannerText'>
           <h1>Creative motion projects</h1>
           <h4>Discover client work to pitch on.</h4>
